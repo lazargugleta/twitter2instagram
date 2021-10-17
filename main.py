c8d3f1342281bc1fa105d468c12929cb2d33562e
Veloce <lazar.gugleta@gmail.com>
2021-10-17 23:00:45 +0200

    use unsplash rest api

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-from searchtweets import load_credentials, collect_results, gen_rule_payload
+# from searchtweets import load_credentials, collect_results, gen_rule_payload
 
-# load_credentials(filename="./search_tweets_creds_example.yaml",
-#                  yaml_key="search_tweets_ent_example",
+# # load_credentials(filename="./search_tweets_creds_example.yaml",
+# #                  yaml_key="search_tweets_ent_example",
+# #                  env_overwrite=False)
+
+
+# premium_search_args = load_credentials(filename="./search_tweets_creds.yaml",
+#                  yaml_key="search_tweets_premium",
 #                  env_overwrite=False)
 
-
-premium_search_args = load_credentials(filename="./search_tweets_creds.yaml",
-                 yaml_key="search_tweets_premium",
-                 env_overwrite=False)
-
-rule = gen_rule_payload("from:orangebook_", results_per_call=10) # testing with a sandbox account
-print(rule)
+# rule = gen_rule_payload("from:orangebook_", results_per_call=10) # testing with a sandbox account
+# print(rule)
 
 
-tweets = collect_results(rule,
-                         max_results=10,
-                         result_stream_args=premium_search_args) # change this if you need to
+# tweets = collect_results(rule,
+#                          max_results=10,
+#                          result_stream_args=premium_search_args) # change this if you need to
 
-[print(tweet.all_text, end='\n\n') for tweet in tweets[0:10]]
+# [print(tweet.all_text, end='\n\n') for tweet in tweets[0:10]]
+
+
+# unsplash part
+
+import requests
+from PIL import Image
+from io import BytesIO
+
+
+def linkFetch():
+    query = "office"
+    orientation = "squarish"
+    url = "https://api.unsplash.com/photos/random?topics=spirituality&content_filter=high&query=people&orientation=squarish&client_id=4PpAlHOuRvO4mse8lIdiTVGUcKUgpNKHVcVSHReIhP0"
+    response = requests.get(url, params=query)
+    # print(response.json()["results"]["urls"])
+    data = response.json()["urls"]["regular"]
+    return data
+
+img_url = linkFetch()
+response = requests.get(img_url)
+img = Image.open(BytesIO(response.content))
+img.show()
 
 
 
@@ -36,3 +58,13 @@ tweets = collect_results(rule,
 # grab the endpoint link
 # https://developer.twitter.com/en/docs/twitter-api/premium/search-api/overview
 # https://api.twitter.com/1.1/tweets/search/30day/my_env_name.json   (replace my_env_name with the one you created in your dev environment)
+
+
+# unsplash
+# register for unsplash developers
+# https://unsplash.com/developers
+
+# register app
+# https://unsplash.com/oauth/applications
+
+# make requests using rest api
