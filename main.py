032b4376191e1f126a242354146e98f23af17c89
Veloce <lazar.gugleta@gmail.com>
2021-10-18 00:25:50 +0200

    center the text

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from searchtweets import load_credentials, collect_results, gen_rule_payload
 #                  env_overwrite=False)
 
 
-premium_search_args = load_credentials(filename="./search_tweets_creds.yaml",
-                 yaml_key="search_tweets_premium",
-                 env_overwrite=False)
+# premium_search_args = load_credentials(filename="./search_tweets_creds.yaml",
+#                  yaml_key="search_tweets_premium",
+#                  env_overwrite=False)
 
-rule = gen_rule_payload("from:orangebook_", results_per_call=10) # testing with a sandbox account
-print(rule)
+# rule = gen_rule_payload("from:orangebook_", results_per_call=10) # testing with a sandbox account
+# print(rule)
 
 
-tweets = collect_results(rule,
-                         max_results=10,
-                         result_stream_args=premium_search_args) # change this if you need to
+# tweets = collect_results(rule,
+#                          max_results=10,
+#                          result_stream_args=premium_search_args) # change this if you need to
 
-[print(tweet.all_text, end='\n\n') for tweet in tweets[0:10]]
+# [print(tweet.all_text, end='\n\n') for tweet in tweets[0:10]]
 
 
 # unsplash part
@@ -34,7 +34,6 @@ def linkFetch():
     orientation = "squarish"
     url = "https://api.unsplash.com/photos/random?topics=spirituality&content_filter=high&query=people&orientation=squarish&client_id=4PpAlHOuRvO4mse8lIdiTVGUcKUgpNKHVcVSHReIhP0"
     response = requests.get(url, params=query)
-    # print(response.json()["results"]["urls"])
     data = response.json()["urls"]["regular"]
     return data
 
@@ -42,14 +41,17 @@ img_url = linkFetch()
 response = requests.get(img_url)
 img = Image.open(BytesIO(response.content))
 width, height = img.size
-x, y = (width/4, height-height/2)
-xx, yy = (width/4, height-height/2)
+print('img.width', width, 'img.height', height)
+x, y = (width/2, height-height/2)
 edited_img = ImageDraw.Draw(img)
 fnt = ImageFont.truetype("Ubuntu-R.ttf", 30)
-text = tweets[1].all_text
+# text = tweets[1].all_text
+text = "Hello world"
+# TODO: add new line in text if it is too long 
 w, h = fnt.getsize(text)
-edited_img.rectangle((x-5, y-5, x + 5 + w, y + 5 + h), fill='grey')
-edited_img.text((x,y), text, font=fnt, fill="white", align="center")
+print('w', w, 'h', h)
+edited_img.rectangle((x-5-w/2, y-5, x + 5 + w/2, y + 5 + h), fill='grey')
+edited_img.text((x-w/2,y), text, font=fnt, fill="white", align="center")
 img.show()
 
 
